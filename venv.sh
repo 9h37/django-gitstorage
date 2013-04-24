b74f4ec39d8425fdb90a2e2e9533e56e4fa1b90d
David Delassus <david.jose.delassus@gmail.com>
2013-04-24 11:01:35 +0200

    Install pygit2 via easy_install instead of pip

--- venv.sh
+++ venv.sh
@@ -15,11 +15,13 @@ then
 
      echo "-- Installing dependencies in virtual environment..."
      pip install -r requirements.txt
+     easy_install pygit2
 else
      echo "-- Activating virtual environment: $vdir..."
      . $vdir/bin/activate || exit 1
 
      echo "-- Updating virtual environment: $vdir..."
      pip install -U -r requirements.txt
+     easy_install -U pygit2
 fi
 
