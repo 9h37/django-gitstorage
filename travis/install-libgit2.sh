8e4637d4c01d5d6545eff0333edcd7871b7961d6
David Delassus <david.jose.delassus@gmail.com>
2013-04-24 16:41:28 +0200

    [travis] clone the same version for libgit2 and pygit2

--- travis/install-libgit2.sh
+++ travis/install-libgit2.sh
@@ -2,8 +2,10 @@
 
 cd ~
 
-git clone --depth=1 -b development https://github.com/libgit2/libgit2.git
+git clone https://github.com/libgit2/libgit2.git
 cd libgit2/
+
+git checkout v0.17.0
 
 mkdir __build__ && cd __build__
 cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
